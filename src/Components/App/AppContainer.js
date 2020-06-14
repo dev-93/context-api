5c130baf9decb1f3399d9886c6ca0a81dc5912dc
dev-93 <cjb940103@gmail.com>
2020-06-14 16:49:57 +0900

    creating the store and Consuming the Store

---
+++ src/Components/App/AppContainer.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import AppPresenter from "./AppPresenter";
+import Store from "../../store";
+
+class AppContainer extends Component {
+    state = {
+        message: "Hello",
+    };
+    componentDidMount = () => {
+        setTimeout(() => {
+            this.setState({
+                message: "BYE",
+            });
+        }, 2000);
+    };
+    render() {
+        return (
+            <Store.Provider value={this.state.message}>
+                <AppPresenter />
+            </Store.Provider>
+        );
+    }
+}
+
+export default AppContainer;
